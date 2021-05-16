fb57386753eee8a55f04b3cd7a86a26a4f3af1bb
newsyp <75907910+newsyp@users.noreply.github.com>
2021-05-16 19:46:01 -0400

    Update and rename cli/src/main/java/bisq/cli/CryptoCurrencyUtil.java to cli/src/main/java/haveno/cli/CryptoCurrencyUtil.java

--- cli/src/main/java/haveno/cli/CryptoCurrencyUtil.java
+++ cli/src/main/java/haveno/cli/CryptoCurrencyUtil.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of Bisq.
+ * This file is part of Haveno.
  *
  * Bisq is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
@@ -15,7 +15,7 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.cli;
+package haveno.cli;
 
 import java.util.ArrayList;
 import java.util.List;
