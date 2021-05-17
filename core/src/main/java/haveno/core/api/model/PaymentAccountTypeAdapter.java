525e0f8df3e232db2738d321a7f57e4019a53cc4
newsyp <75907910+newsyp@users.noreply.github.com>
2021-05-17 17:49:01 -0400

    Update and rename core/src/main/java/bisq/core/api/model/PaymentAccountTypeAdapter.java to core/src/main/java/haveno/core/api/model/PaymentAccountTypeAdapter.java

--- core/src/main/java/haveno/core/api/model/PaymentAccountTypeAdapter.java
+++ core/src/main/java/haveno/core/api/model/PaymentAccountTypeAdapter.java
@@ -15,16 +15,16 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.api.model;
+package haveno.core.api.model;
 
 
-import bisq.core.locale.Country;
-import bisq.core.locale.FiatCurrency;
-import bisq.core.locale.TradeCurrency;
-import bisq.core.payment.CountryBasedPaymentAccount;
-import bisq.core.payment.MoneyGramAccount;
-import bisq.core.payment.PaymentAccount;
-import bisq.core.payment.payload.PaymentAccountPayload;
+import haveno.core.locale.Country;
+import haveno.core.locale.FiatCurrency;
+import haveno.core.locale.TradeCurrency;
+import haveno.core.payment.CountryBasedPaymentAccount;
+import haveno.core.payment.MoneyGramAccount;
+import haveno.core.payment.PaymentAccount;
+import haveno.core.payment.payload.PaymentAccountPayload;
 
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
@@ -50,13 +50,13 @@ import java.lang.reflect.Method;
 
 import lombok.extern.slf4j.Slf4j;
 
-import static bisq.common.util.ReflectionUtils.*;
-import static bisq.common.util.Utilities.decodeFromHex;
-import static bisq.core.locale.CountryUtil.findCountryByCode;
-import static bisq.core.locale.CurrencyUtil.getAllTransferwiseCurrencies;
-import static bisq.core.locale.CurrencyUtil.getCurrencyByCountryCode;
-import static bisq.core.locale.CurrencyUtil.getTradeCurrencies;
-import static bisq.core.locale.CurrencyUtil.getTradeCurrenciesInList;
+import static haveno.common.util.ReflectionUtils.*;
+import static haveno.common.util.Utilities.decodeFromHex;
+import static haveno.core.locale.CountryUtil.findCountryByCode;
+import static haveno.core.locale.CurrencyUtil.getAllTransferwiseCurrencies;
+import static haveno.core.locale.CurrencyUtil.getCurrencyByCountryCode;
+import static haveno.core.locale.CurrencyUtil.getTradeCurrencies;
+import static haveno.core.locale.CurrencyUtil.getTradeCurrenciesInList;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.lang.String.format;
 import static java.util.Arrays.stream;
