d3742469a456f34ed1967d3c0e9a39343129b39a
newsyp <75907910+newsyp@users.noreply.github.com>
2021-05-16 20:18:56 -0400

    Update and rename cli/src/main/java/bisq/cli/opts/GetAddressBalanceOptionParser.java to cli/src/main/java/haveno/cli/opts/GetAddressBalanceOptionParser.java

--- cli/src/main/java/haveno/cli/opts/GetAddressBalanceOptionParser.java
+++ cli/src/main/java/haveno/cli/opts/GetAddressBalanceOptionParser.java
@@ -1,5 +1,5 @@
 /*
- * This file is part of Bisq.
+ * This file is part of Haveno.
  *
  * Bisq is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by
@@ -15,12 +15,12 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.cli.opts;
+package haveno.cli.opts;
 
 
 import joptsimple.OptionSpec;
 
-import static bisq.cli.opts.OptLabel.OPT_ADDRESS;
+import static haveno.cli.opts.OptLabel.OPT_ADDRESS;
 
 public class GetAddressBalanceOptionParser extends AbstractMethodOptionParser implements MethodOpts {
 
