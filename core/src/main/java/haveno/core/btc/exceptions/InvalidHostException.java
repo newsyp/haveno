c32a83fb1c9be1f79c96c38ffbc444c9ef99ee09
newsyp <75907910+newsyp@users.noreply.github.com>
2021-05-17 18:50:48 -0400

    Update and rename core/src/main/java/bisq/core/btc/exceptions/InvalidHostException.java to core/src/main/java/haveno/core/btc/exceptions/InvalidHostException.java

--- core/src/main/java/haveno/core/btc/exceptions/InvalidHostException.java
+++ core/src/main/java/haveno/core/btc/exceptions/InvalidHostException.java
@@ -15,7 +15,7 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.btc.exceptions;
+package haveno.core.btc.exceptions;
 
 public class InvalidHostException extends IllegalArgumentException {
 
