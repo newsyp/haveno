c90c7e43c106592a08e2aa277df2d5ae34ff0447
newsyp <75907910+newsyp@users.noreply.github.com>
2021-05-17 23:04:03 -0400

    Update and rename core/src/main/java/bisq/core/exceptions/TradePriceOutOfToleranceException.java to core/src/main/java/haveno/core/exceptions/TradePriceOutOfToleranceException.java

--- core/src/main/java/haveno/core/exceptions/TradePriceOutOfToleranceException.java
+++ core/src/main/java/haveno/core/exceptions/TradePriceOutOfToleranceException.java
@@ -15,7 +15,7 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.exceptions;
+package haveno.core.exceptions;
 
 public class TradePriceOutOfToleranceException extends Exception {
     public TradePriceOutOfToleranceException(String message) {
