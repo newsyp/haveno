fd7e04fe524ceda11067eb93bbb6986590ca6888
newsyp <75907910+newsyp@users.noreply.github.com>
2021-05-17 23:15:34 -0400

    Update and rename core/src/main/java/bisq/core/monetary/Altcoin.java to core/src/main/java/haveno/core/monetary/Altcoin.java

--- core/src/main/java/haveno/core/monetary/Altcoin.java
+++ core/src/main/java/haveno/core/monetary/Altcoin.java
@@ -15,9 +15,9 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.monetary;
+package haveno.core.monetary;
 
-import bisq.core.util.ParsingUtils;
+import haveno.core.util.ParsingUtils;
 
 import org.bitcoinj.core.Monetary;
 import org.bitcoinj.utils.MonetaryFormat;
