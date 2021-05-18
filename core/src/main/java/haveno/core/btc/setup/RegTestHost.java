2dfb8f676fcf08f228a06e27fd521d70eb45ea18
newsyp <75907910+newsyp@users.noreply.github.com>
2021-05-17 22:10:15 -0400

    Update and rename core/src/main/java/bisq/core/btc/setup/RegTestHost.java to core/src/main/java/haveno/core/btc/setup/RegTestHost.java

--- core/src/main/java/haveno/core/btc/setup/RegTestHost.java
+++ core/src/main/java/haveno/core/btc/setup/RegTestHost.java
@@ -15,9 +15,9 @@
  * along with Bisq. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package bisq.core.btc.setup;
+package haveno.core.btc.setup;
 
-import bisq.common.config.Config;
+import haveno.common.config.Config;
 
 public enum RegTestHost {
 
